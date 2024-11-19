faa1f1ef51fb73977464e0309657b5cfa3b330d9
Asad0018 <dangerasad259@gmail.com>
2024-11-19 21:16:47 +0330

    Arrange 3 numbers
    Takes 3 numbers and arranges them from large to small.
    I have uploaded the initial version of it in the homework repository. This one is a little bit more professional.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Arrange
+{
+    class Program
+    {
+        static double Get()
+        {
+            Console.WriteLine("Enter a number");
+            return double.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            double[] nums = { Get(), Get(), Get() };
+            Array.Sort(nums);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sorted Numbers from large to small:");
+            for (int i = nums.Length; i > 0; i--) Console.Write(nums[i - 1] + " , ");
+            Console.ReadKey();
+        }
+    }
+}
