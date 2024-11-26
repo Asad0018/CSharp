e9ba9f3aeb0c291a114dbd97fb94dc7cf6a3006b
Asad0018 <dangerasad259@gmail.com>
2024-11-26 20:49:18 +0330

    Prime Number
    بررسی می‌کند که آیا عدد ورودی اول است یا خیر

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a natural number");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; true; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Not Prime");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("It's Prime");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
