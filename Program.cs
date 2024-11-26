7405596193356765679b9c0c67b2316ca06e07a8
Asad0018 <dangerasad259@gmail.com>
2024-11-26 20:51:56 +0330

    PrimeNumber.cs
    بررسی می‌کند که آیا عدد ورودی اول است یا خیر

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//       +---------------------------------------------+
+//       | بررسی می‌کند که آیا عدد ورودی اول است یا خیر          |
+//       +---------------------------------------------+
 namespace _08_PrimeNumber
 {
     class Program
