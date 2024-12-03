235c7c4b755c1bdb463c17713f780f6d59d89286
Asad0018 <dangerasad259@gmail.com>
2024-12-03 09:36:05 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//          +-----------------------------+       
+//          |          Variables          |
+//          +-----------------------------+
+            double num1;
+            double num2;
+            double num3;
+
+//          +-----------------------------+       
+//          |           Inputs            |
+//          +-----------------------------+
+            Console.WriteLine("Enter a number");
+            string a = Console.ReadLine();
+            num1 = double.Parse(a);
+
+            Console.WriteLine("Now enter an other");
+            string b = Console.ReadLine();
+            num2 = double.Parse(b);
+
+            Console.WriteLine("And an other one");
+            string c = Console.ReadLine();
+            num3 = double.Parse(c);
+
+//          +-----------------------------+       
+//          |           Output            |
+//          +-----------------------------+
+            double Average = (num1 + num2 + num3) / 3;
+            Console.WriteLine(Average);
+
+//       ---End-----------------------------       
+            Console.ReadKey();
+
+        }
+    }
+}
