47d50f3e389b85340f1a8c4a8f8b147654d0562a
Asad0018 <dangerasad259@gmail.com>
2024-12-03 09:45:33 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_CountLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + ", " + name.Length + " Letters");
+            Console.ReadKey();
+        }
+    }
+}
