c853046213ecf2f70ae162f7edfb6e8f2681e956
Asad0018 <dangerasad259@gmail.com>
2024-12-03 09:39:10 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_HiBye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            if (20 <= age && age <= 30)
+                Console.WriteLine("Hi");
+            else
+                Console.WriteLine("Bye");
+            Console.ReadKey();
+        }
+    }
+}
