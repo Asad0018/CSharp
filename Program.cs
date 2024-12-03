368f342f5b47cabc46b01092932d868f0e6450e3
Asad0018 <dangerasad259@gmail.com>
2024-12-03 09:43:23 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a natural number");
+            int num = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            for (int j = 1; j <= num; j++)
+            {
+                Console.WriteLine();
+                for (int i = 1; i <= num; i++)
+                    Console.Write(i*j + ",");
+            }
+            Console.ReadKey();
+        }
+    }
+}
