fb05b4f7a1cbdf211fed8cd6f4637005b3b6c02d
Asad0018 <dangerasad259@gmail.com>
2024-12-03 09:42:28 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a natural number");
+            int num = int.Parse(Console.ReadLine());
+            int fib1 = 0, fib2 = 1;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(fib1 + ", " + fib2 + ", ");
+            for (int i = 3; i <= num; i++)
+            {
+                int fib3 = fib1 + fib2;
+                Console.Write(fib3 + ", ");
+                fib1 = fib2;
+                fib2 = fib3;
+            }
+            Console.ReadKey();
+        }
+    }
+}
